5e601a0a8494c073a82ebfaf1d45b4620fc2abf8
tal3hh <suleymanovtaleh887@gmail.com>
2023-11-04 13:32:46 +0400

    RabbitMQ default create

---
+++ RabbitMQ.Default/Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+//Uri appsetting.json'da saxlamaq daha uygundur.
+factory.Uri = new Uri("amqps://bheuyprq:o4Ro9yF4IFODJAGCQS-Sctz9LqeKf3hW@lionfish.rmq.cloudamqp.com/bheuyprq");
+
+//Baglanti yaradilir.
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue yaradiriq. Producer terefdeki ile eyni olmalidir.
+channel.QueueDeclare(queue: "default-queue", exclusive: false);
+
+//Mesaji/Datani almaq
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(queue: "default-queue", autoAck: true, consumer: consumer);
+
+
+consumer.Received += Consumer_Received;
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{   
+    //Mesaj byte tipinde gelir ve onu stringe stringe yaxud gonderilen mesaja gore dto,modele cevirib uygun isleri gore bilerik. 
+    var byteMessage = e.Body.Span;
+    string data = Encoding.UTF8.GetString(byteMessage);
+
+    Console.WriteLine("Gelen Mesaj: " + data);
+}
+
+Console.Read();
---
+++ RabbitMQ.Default/Producer/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+//Uri appsetting.json'da saxlamaq daha uygundur.
+factory.Uri = new Uri("amqps://bheuyprq:o4Ro9yF4IFODJAGCQS-Sctz9LqeKf3hW@lionfish.rmq.cloudamqp.com/bheuyprq");
+
+//Baglanti yaradilir.
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue(quruq) yaradiriq.
+channel.QueueDeclare(queue: "default-queue", exclusive: false);
+
+//Queue(quyruga) mesaj gondermek.
+Console.Write("Gonderilen Mesaj: ");
+string message = Console.ReadLine();
+
+byte[] data = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "", routingKey: "default-queue", body: data);
+
+Console.Read();
+
+
