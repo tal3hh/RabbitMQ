4f78b5481b5cff5389948c54e116a686478307d5
tal3hh <suleymanovtaleh887@gmail.com>
2023-11-05 12:00:23 +0400

    topic add

---
+++ RabbitMQ.Topic/Consumer3/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://bheuyprq:o4Ro9yF4IFODJAGCQS-Sctz9LqeKf3hW@lionfish.rmq.cloudamqp.com/bheuyprq");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Exchange tipi ve adini Producerdeki ile eyni olmalidir.
+channel.ExchangeDeclare(exchange: "topic-test", type: ExchangeType.Topic);
+
+
+//RoutingKey'e verilecek adin nece olacagini urda teyin edek.
+string topicName = "green.#";
+Console.WriteLine(topicName);
+
+//Queue yaradaraq bu quyruga datalari ataciyiq.
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(queue: queueName,
+                  exchange: "topic-test",
+                  routingKey: topicName);
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (model, e) =>
+{
+    Console.WriteLine("Gelen mesaj: " + Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ.Topic/Producer/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://bheuyprq:o4Ro9yF4IFODJAGCQS-Sctz9LqeKf3hW@lionfish.rmq.cloudamqp.com/bheuyprq");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Exchange tipi ve adi
+channel.ExchangeDeclare(exchange: "topic-test", type: ExchangeType.Topic);
+
+
+//Mesaj gonder.
+Console.Write("Mesaj: ");
+string message = Console.ReadLine();
+byte[] data = Encoding.UTF8.GetBytes(message);
+
+
+//Publis etdikde, routingKey adina diqqet etmek lazimdir.Cunki ona uygun data alinacaqdir.(* ve #)
+channel.BasicPublish(exchange: "topic-test",
+                     routingKey: "green.red",
+                     body: data);
+
+Console.Read();
